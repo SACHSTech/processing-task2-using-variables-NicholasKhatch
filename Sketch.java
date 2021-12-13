9ddd58e7b11337749bee6b04d029c03e6e9e4245
NicholasKhatch <nicholas.khatchadourian23@ycdsbk12.ca>
2021-12-13 18:23:38 -0500

    final version 1, using variables

--- Sketch.java
+++ Sketch.java
@@ -8,7 +8,7 @@ public class Sketch extends PApplet {
    */
   public void settings() {
 	// put your size call here
-    size(400, 400);
+    size(600, 600);
   }
 
   /** 
@@ -24,26 +24,26 @@ public class Sketch extends PApplet {
    */
   public void draw() {
     stroke(35, 92, 35);
-    line(200,240,240,260);
+    line(width/2, (float) (height/1.66666667), (float) (width/1.6666667), (float) (height/1.538461));
 
     stroke(35, 92, 35);
-    line(150,220,200,200);
+    line((float) (width/2.66666667), (float) (height/1.81818181), width/2, height/2);
 
     stroke(35, 92, 35);
     fill(60, 179, 45);
-    rect(180, 200, 40, 150);
+    rect((float) (width*.45), height/2, width/10, (float) (height/2.66666667));
 
     stroke(0, 0, 0);
     fill(255,0,0);
-    ellipse(200,150,150,150);
+    ellipse(width/2, (float) (height/2.66666667), (float) (width/2.66666667), (float) (height/2.66666667));
 
     stroke(0, 0, 0);
-    fill(0,0,0);
-    ellipse(200,150,65,65);
+    fill(0, 0, 0);
+    ellipse(width/2, (float) (height/2.66666667), (float) (width/6.15384615), (float) (height/6.15384615));
 
     stroke(0, 0, 0);
     fill(63, 122, 55);
-    rect(0,350,400,50);
+    rect(width - width, (float) (height/1.14285714), width, height/8);
   }
   
   // define other methods down here.
